17046abe6c339e6d47ed920a9c8420289f7a8431
RandilW <85533296+RandilW@users.noreply.github.com>
2023-02-19 01:58:48 -0600

    update

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+let submitButton = document.querySelector('button');
+let input = document.querySelector('input');
+let form = document.querySelector('form');
+let arrow = document.querySelector('.icon-arrow');
+let close = document.querySelector('.icon-error');
+let check = document.querySelector('.icon-success');
+let checkPath = document.querySelector('.icon-success path');
+
+
+
 const taskList = document.getElementById('task-list');
-const form = document.getElementById('form');
 const taskInput = document.getElementById('task-input');
 const addBtn = document.getElementById('add-btn');
+
+
 
 // Check for tasks in local storage
 let tasks;
@@ -14,19 +25,63 @@ if (localStorage.getItem('tasks') === null) {
   });
 }
 
-// Event listener for form submit
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  const taskName = taskInput.value.trim();
 
-  if (taskName === '') {
-    alert('Please enter a task name');
-  } else {
-    createTaskElement(taskName);
-    tasks.push(taskName);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    taskInput.value = '';
+
+console.log(checkPath.getTotalLength());
+submitButton.addEventListener('click', e => {
+  e.preventDefault();
+  if (form.dataset.state != "typing") {
+    TweenMax.to(close, 0.5, {
+      opacity: 0,
+      rotation: 0 });
+
+    TweenMax.fromTo(check, 0.5, {
+      rotation: 360 * 3 },
+    {
+      rotation: 0 });
+
+    TweenMax.to(checkPath, 0.5, {
+      strokeDashoffset: 175 });
+
+    form.dataset.state = "typing";
+    TweenMax.fromTo(arrow, 0.45, {
+      rotation: 0,
+      x: '-60px',
+      opacity: '0' },
+    {
+      rotation: 0,
+      x: '0',
+      opacity: '1' });
+
+    return;
   }
+
+  TweenMax.to(arrow, 1, {
+    rotation: 360 * 5,
+    transformOrigin: "50% 50%",
+    opacity: 0 });
+
+
+  setTimeout(() => {
+    if (input.value == '') {
+      form.dataset.state = "error";
+      // alert('Please enter a task name');
+      TweenMax.to(close, 0.5, {
+        rotation: 360 * 3,
+        opacity: 1 });
+
+    } else {
+      form.dataset.state = "success";
+      TweenMax.to(checkPath, 0.65, {
+        strokeDashoffset: 0 });
+
+        const taskName = taskInput.value.trim();
+        createTaskElement(taskName);
+        tasks.push(taskName);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        taskInput.value = '';
+    }
+  }, 500);
 });
 
 // Function to create a new task element
